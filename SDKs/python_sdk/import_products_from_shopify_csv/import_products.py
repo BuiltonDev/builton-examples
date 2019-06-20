1ff022d0fff7fa6e86d9e06050b5ed34e62d076d
GabrielBlondeau-Kirby <blondeau.gabriel@gmail.com>
2019-06-20 09:47:50 +0200

    improve the code, more logs

--- SDKs/python_sdk/import_products_from_shopify_csv/import_products.py
+++ SDKs/python_sdk/import_products_from_shopify_csv/import_products.py
@@ -33,6 +33,7 @@ def main(api_key=None, bearer_token=None):
     bearer_token = bearer_token if bearer_token is not None else BEARER_TOKEN
 
     builton = Builton(api_key=api_key, bearer_token=bearer_token, endpoint="https://qa.kvass.ai")
+    cprint("BuiltOn skd is ready!", "cyan")
 
     import_products(builton)
 
@@ -41,31 +42,66 @@ def import_products(builton: Builton):
     with open('products_export.csv', newline='') as csv_product_file:
         csv_reader = csv.DictReader(csv_product_file, delimiter=CSV_DELIMITER)
         for shopify_product in csv_reader:
-            properties = {
-                'vendor': shopify_product.get('Vendor'),
-                'grams': shopify_product.get('Variant Grams'),
-                'weight_unit': shopify_product.get('Variant Weight Unit')
-            }
-
-            tags = [tag for tag in shopify_product.get('Tags').split(',')]
-            tags.append(shopify_product.get('Type'))
-
-            body = {
-                'active': parse_boolean_param(shopify_product.get('Published')),
-                'name': shopify_product.get('Title'),
-                'description': shopify_product.get('Body (HTML)'),
-                'price': float(shopify_product.get('Variant Price')),
-                'external_reference': shopify_product.get('Handle'),
-                'tags': tags,
-                'properties': properties,
-                'image_url': shopify_product.get('Image Src')
-            }
-
+            cprint("============================================================")
             try:
+                cprint("======================== Properties ========================")
+                vendor = shopify_product.get('Vendor')
+                cprint("Vendor: %s" % vendor, "cyan")
+                grams = shopify_product.get('Variant Grams')
+                cprint("Grams: %s" % grams, "cyan")
+                weight_unit = shopify_product.get('Variant Weight Unit')
+                cprint("Weight Unit: %s" % weight_unit, "cyan")
+
+                properties = {
+                    'vendor': vendor,
+                    'grams': grams,
+                    'weight_unit': weight_unit
+                }
+
+                cprint("=========================== Tags ===========================")
+                tags = [tag for tag in shopify_product.get('Tags').split(',')]
+                tags.append(shopify_product.get('Type'))
+                cprint("Tags: %s" % tags, "cyan")
+
+                cprint("=========================== Other ===========================")
+                active = parse_boolean_param(shopify_product.get('Published'))
+                cprint("Active: %s" % active, "cyan")
+                name = shopify_product.get('Title')
+                cprint("Name: %s" % name, "cyan")
+                description = shopify_product.get('Body (HTML)')
+                cprint("Description: %s" % description, "cyan")
+                price = float(shopify_product.get('Variant Price'))
+                cprint("Price: %s" % price, "cyan")
+                external_reference = shopify_product.get('Handle')
+                cprint("Handle | External Reference: %s" % external_reference, "cyan")
+                image_url = shopify_product.get('Image Src')
+                cprint("Image Url: %s" % image_url, "cyan")
+
+                body = {
+                    'active': active,
+                    'name': name,
+                    'description': description,
+                    'price': price,
+                    'external_reference': external_reference,
+                    'tags': tags,
+                    'properties': properties,
+                    'image_url': image_url
+                }
+
                 builton.product().create(body=body)
+                cprint("================== SUCCESSFULLY IMPORTED ===================", "green")
+
             except Exception as error:
-                cprint("something wrong happened: %s" % error, "orange")
+                cprint("========================== ERROR ===========================", "red")
+                cprint("something wrong happened: %s" % error, "red")
+                cprint("product handle: %s" % shopify_product.get('Handle'), "yellow")
+                cprint("product csv: %s" % shopify_product, "yellow")
+                cprint("============================================================")
+
                 continue
+        cprint("============================================================")
+        cprint("======================= IMPORT DONE ========================")
+        cprint("============================================================")
 
 
 def parse_boolean_param(param):
@@ -77,6 +113,7 @@ def parse_boolean_param(param):
 
 
 if __name__ == "__main__":
+    cprint("Let's do it!", "cyan")
     api_key = input("Your API Key:")
     bearer_token = input("Your Service Account Key:")
 
